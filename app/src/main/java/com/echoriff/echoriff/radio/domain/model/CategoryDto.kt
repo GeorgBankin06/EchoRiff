0058d87ce57a4815e820112ce5f050956b0128e4
Georg Bankin <georgio3383@gmail.com>
2024-11-07 22:39:08 +0200

    Implement domain models for radio feature

---
+++ app/src/main/java/com/echoriff/echoriff/radio/domain/model/CategoryDto.kt
@@ -0,0 +1,7 @@
+package com.echoriff.echoriff.radio.domain.model
+
+data class CategoryDto(
+    val bgImgUrl: String,
+    val radios: List<RadioDto>,
+    val title: String
+)
---
+++ app/src/main/java/com/echoriff/echoriff/radio/domain/model/RadioDto.kt
@@ -0,0 +1,9 @@
+package com.echoriff.echoriff.radio.domain.model
+
+data class RadioDto (
+    val coverArtUrl: String,
+    val streamUrl: String,
+    val title: String,
+    val webUrl: String?,
+    val intro: String
+)
