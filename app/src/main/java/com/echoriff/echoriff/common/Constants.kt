1baaa9526477dfa2b60d02abc8325e023e739c9a
Georg Bankin <georgio3383@gmail.com>
2024-12-08 21:30:54 +0200

    Added files to .gitignore

--- app/src/main/java/com/echoriff/echoriff/common/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.echoriff.echoriff.common
-
-object Constants {
-    const val USERS: String = "Users"
-    const val ADMIN_EMAIL = "georgio3383@gmail.com"
-    const val API_TOKEN = "b3f34d3fca2455e7a85d2b9f20abc1db"
-}
